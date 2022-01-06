5d80e2b89c3966c34e66ed7aea0234d7dab9610a
Riddhi-Chatterjee <riddhich@rediffmail.com>
2022-01-06 18:35:09 +0530

    Added more firearms classes

---
+++ src/BubbleFire.cpp
@@ -0,0 +1,77 @@
+#include "BubbleFire.h"
+
+BubbleFire::BubbleFire(Vec2D p, Vec2D v)
+	: propagation("BubbleProp.bmp", 1, 1), burst("BubbleBurst.bmp", 1, 2), Object(p, v, 0.15, 10, 10, 1)
+{
+    count = 0;
+    timepassed = 0.0;
+    destroyed = false;
+	propagation.selectSprite(count,0);
+    burst.selectSprite(0,0);
+    currentSpriteSheet = 0;
+    vel.x = v.x;
+    vel.y = v.y;
+    initialVel = {v.x, v.y};
+
+	m_position.x = 0;
+	m_position.y = 0;
+	m_position.w = getLength();
+	m_position.h = getBredth();
+}
+
+
+int BubbleFire::fire(double delta_time)
+{
+    pos.x = pos.x + (delta_time*vel.x);
+    pos.y = pos.y + (delta_time*vel.y);
+    if(!destroyed)
+    {
+	    timepassed += delta_time;
+        if(((initialVel.x != this->vel.x) || (initialVel.y != this->vel.y)) && currentSpriteSheet == 0)
+        {
+            currentSpriteSheet = 1;
+            timepassed = 0.0;
+            count = 0;
+            burst.selectSprite(count,0);
+        }
+        
+        if(currentSpriteSheet == 1)
+        {
+            if(timepassed >= 0.5)
+            {
+                timepassed = 0.0;
+                count = (count + 1);
+                if(count == 2)
+                {
+                    destroyed = true;
+                    return 0;
+                }
+                else
+                {
+                    burst.selectSprite(count,0);
+                }
+            }   
+        }
+    }
+    return 1;
+}
+
+
+void BubbleFire::draw(SDL_Surface* surface, int x, int y)
+{
+	m_position.x = pos.x;
+	m_position.y = pos.y;
+	if(currentSpriteSheet == 0)
+    {
+        propagation.drawSelectedSprite(surface, &m_position);
+    }
+    else if(currentSpriteSheet == 1)
+    {
+        burst.drawSelectedSprite(surface, &m_position);
+    }
+}
+
+
+BubbleFire::~BubbleFire()
+{
+}
---
+++ src/SpiralFire.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <SDL.h>
+#include "Spritesheet.h"
+#include <iostream>
+#include "Physics.h"
+#include "Enemy.h"
+
+class SpiralFire : public Object
+{
+
+private:
+	Spritesheet propagation, burst;
+	SDL_Rect m_position;
+	double timepassed;
+    int currentSpriteSheet;
+    Vec2D initialVel;
+    int count;
+    bool destroyed;
+	
+public:
+	SpiralFire(Vec2D p, Vec2D v);
+	~SpiralFire();
+
+	void update(double delta_time) {}
+    int fire(double delta_time);
+	void draw(SDL_Surface* surface, int x, int y) override;
+	void handle_events(SDL_Event const& event) {}
+	bool isGravity() override { return false; }
+	bool isCollideable() const { return true; }
+};
